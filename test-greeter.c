d2da9a8de792481a70f381268eaf7360d42a1eb0
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 17:11:43 +0100

    Add forgotten colon to TestGreeter

--- test-greeter.c
+++ test-greeter.c
@@ -3,7 +3,7 @@
 TestGreeterCountUnit test_greeter_greet_count = 0;
 
 /**
- * TestGreeter::TestGreeter
+ * TestGreeter::TestGreeter:
  * @short_description: This is a very helpful greeting API.
  * @title: The Test Greeter
  *
